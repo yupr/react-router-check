da2b588765864f8c2b81ab07ca5958697513740e
yupr <12003186+yupr@users.noreply.github.com>
2025-10-13 21:27:32 +0900

    GraphQLサーバーを実装

---
+++ graphl-server/prisma/migrations/20251013090827_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- graphl-server/src/server.ts
+++ graphl-server/src/server.ts
@@ -1,20 +1,71 @@
 import { ApolloServer, gql } from "apollo-server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type Context = {
+  prisma: PrismaClient;
+};
 
 const typeDefs = gql`
+  type Todo {
+    id: ID!
+    title: String!
+    completed: Boolean!
+  }
+
   type Query {
-    message: String
+    getTodos: [Todo!]!
+  }
+
+  type Mutation {
+    addTodo(title: String!): Todo!
+    updateTodo(id: ID!, completed: Boolean!): Todo!
+    deleteTodo(id: ID!): Todo!
   }
 `;
 
+type AddTodo = {
+  title: string;
+};
+
 const resolvers = {
   Query: {
-    message: () => "Hello World!",
+    getTodos: async (_: unknown, args: any, context: Context) => {
+      return await context.prisma.todo.findMany();
+    },
+  },
+  Mutation: {
+    addTodo: (_: unknown, { title }: AddTodo, context: Context) => {
+      return context.prisma.todo.create({
+        data: {
+          title,
+          completed: false,
+        },
+      });
+    },
+    updateTodo: (
+      _: unknown,
+      { id, completed }: { id: string; completed: boolean },
+      context: Context
+    ) => {
+      return context.prisma.todo.update({
+        where: { id },
+        data: { completed },
+      });
+    },
+    deleteTodo: (_: unknown, { id }: { id: string }, context: Context) => {
+      return context.prisma.todo.delete({
+        where: { id },
+      });
+    },
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: (): Context => ({ prisma }),
 });
 
 server.listen().then(({ url }) => {
